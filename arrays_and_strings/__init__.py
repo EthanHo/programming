7ce43dee5097fa84cfaf81dcf6234879085de6e7
Ethan Ho <ethanho@MonkeyKing.T-mobile.com>
2017-08-13 16:41:33 -0400

    3 problems

--- arrays_and_strings/__init__.py
+++ arrays_and_strings/__init__.py
@@ -9,8 +9,31 @@ def all_unique(x):
 
 
 def all_unique2(x):
+    for i in range(len(x)-1):
+        for j in range(i+1, len(s)):
+            if x[i]==x[j]:
+                return False
     return True
 
 
-print all_unique("Ethan")
+def permutation(x, y):
+    return sorted(x)==sorted(y)
 
+
+def replace_spaces(s, n):
+    read_index = len(s) - (2 * n) - 1
+    write_index = len(s)-1
+    while read_index >= 0:
+        if s[read_index] != ' ':
+            s[write_index] = s[read_index]
+            write_index -= 1
+            read_index -= 1
+        else:
+            s[write_index] = '0'
+            s[write_index-1] = '2'
+            s[write_index-2] = '%'
+            write_index -= 3
+            read_index -= 1
+    return s
+
+print replace_spaces(list('aa zz  '), 1)
